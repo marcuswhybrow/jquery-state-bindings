6c5aede0409eb7b2dda1cda65a94c8816036ad56
Marcus Whybrow <marcus@marcuswhybrow.net>
2011-03-27 04:34:05 +0100

    Custom events are now fired whenever a state is enabled or disabled.
    For example if the newly enabled state is called "editing", then
    "editing.stateEnabled" would fire, the counterpart "editing.stateDisabled"
    works in the same way.
    
    A state named "editing>moving" can not be enabled unless the "editing" state is
    already enabled.
    
    If "editing>moving" was an active state, disabling the "editing" state would also
    disable "editing>moving" along with any other state starting with "editing>".
    
    States can be chained e.g. "editing>moving>inProgress", the generic rule being
    than no state can be enabled unless its parent is enabled, and no child state
    can remain active if its parent state becomes disabled.

--- jquery.statebindings.js
+++ jquery.statebindings.js
@@ -1,4 +1,5 @@
 (function($) {
+    var $window = $(window);
 
     // Accepts a function or an array of functions an calls an
     // anonymous function with each as the only argument.
@@ -44,21 +45,28 @@
         return str.split(' ');
     }
     
-    function forStateNotIn($obj, existingArray, newArray, f) {
-        for (var j = 0; j < existingArray.length; j++) {
-            // Remove the bindings for the previous state
-            var state = existingArray[j];
-            if (! newArray.contains(state)) {
-                events = $obj.stateBindings[state];
-                for (eventName in events) {
-                    handlers = events[eventName];
-
-                    iterate(handlers, function(handler) {
-                        f($obj, eventName+'.stateBindings.'+state, handler);
-                    });
-                }
+    function forStates($obj, states, f) {
+        for (var i = 0; i < states.length; i++) {
+            var state = states[i];
+            var events = $obj.stateBindings[state];
+            for (eventName in events) {
+                handlers = events[eventName];
+                iterate(handlers, function(handler) {
+                    f(eventName+'.stateBindings.'+state, handler);
+                });
             }
         }
+    }
+    
+    function subtract(arrayOne, arrayTwo) {
+        var newArray = new Array();
+        var count = 0;
+        for (var i = 0; i < arrayOne.length; i++) {
+            if (! arrayTwo.contains(arrayOne[i])) {
+                newArray[count++] = arrayOne[i];
+            }
+        }
+        return newArray;
     }
 
     // jQuery methods
@@ -71,6 +79,8 @@
             objects: [],
 
             liveObjects: [],
+            
+            hierarchySeparator: '>',
 
             // A method to set the state, and as a result of doing so, alter
             // the bindings active on the managed objects.
@@ -83,73 +93,102 @@
                 }
                 var previousStates = $.stateBindings.activeStates;
                 var i, j, events, eventName, handlers;
+                
+                var statesToDisable = subtract(previousStates, newStates);
+                var statesToEnable = subtract(newStates, previousStates);
 
                 // Handle the live bindings
                 for(i = 0; i < $.stateBindings.liveObjects.length; i++) {
                     var $liveObj = $.stateBindings.liveObjects[i];
                     
-                    forStateNotIn(
-                        $liveObj, previousStates, newStates,
-                        function($obj, eventName, handler) {
-                            $obj.die(eventName, handler);
-                        }
-                    )
+                    forStates($liveObj, statesToDisable, function(eventName, handler) {
+                        $liveObj.die(eventName, handler);
+                    });
                     
-                    forStateNotIn(
-                        $liveObj, newStates, previousStates,
-                        function($obj, eventName, handler) {
-                            $obj.live(eventName, handler);
-                        }
-                    )
+                    forStates($liveObj, statesToEnable, function(eventName, handler) {
+                        $liveObj.live(eventName, handler);
+                    });
                 }
 
                 // Handle the normal bindings
                 for (i = 0; i < $.stateBindings.objects.length; i++) {
                     var $obj = $.stateBindings.objects[i];
                     
-                    forStateNotIn(
-                        $obj, previousStates, newStates,
-                        function($obj, eventName, handler) {
-                            $obj.unbind(eventName, handler);
-                        }
-                    )
+                    forStates($obj, statesToDisable, function(eventName, handler) {
+                        $obj.unbind(eventName, handler);
+                    });
                     
-                    forStateNotIn(
-                        $obj, newStates, previousStates,
-                        function($obj, eventName, handler) {
-                            $obj.bind(eventName, handler);
-                        }
-                    )
+                    forStates($obj, statesToEnable, function(eventName, handler) {
+                        $obj.bind(eventName, handler);
+                    });
                 }
-
+                
                 $.stateBindings.activeStates = newStates;
+                
+                for (var i = 0; i < statesToDisable.length; i++) {
+                    $window.trigger(statesToDisable[i]+'.stateDisabled');
+                }
+                
+                for (var i = 0; i < statesToEnable.length; i++) {
+                    $window.trigger(statesToEnable[i]+'.stateEnabled');
+                }
             },
             
+            // Actives a state, as long as its parent state is already active
             addState: function(state) {
                 if ($.stateBindings.activeStates.contains(state)) {
-                    return false;
+                    return true;
                 } else {
-                    var newStates =
-                        $.stateBindings.activeStates.concat([state]);
-                    $.stateBindings.setState(newStates);
+                    
+                    // Find the right most hirerarchy separator character
+                    var rightMostIndex = -1;
+                    while(true) {
+                        var index = state.indexOf(this.hierarchySeparator, rightMostIndex+1);
+                        if (index > -1)
+                            rightMostIndex = index;
+                        else
+                            break;
+                    }
+                    
+                    var parentState = state.substr(0, rightMostIndex);
+                    if (rightMostIndex == -1 || $.stateBindings.isEnabled(parentState)) {
+                        var newStates = $.stateBindings.activeStates.concat([state]);
+                        $.stateBindings.setState(newStates);
+                        return true;
+                    }
                 }
+                return false;
             },
             
+            // Removes any state which starts with the given state name
             removeState: function(state) {
-                var newStates = $.stateBindings.activeStates.subtract(state);
-                $.stateBindings.setState(newStates);
+                if ($.stateBindings.isEnabled(state)) {
+                    var newStates = $.stateBindings.activeStates.subtract(state);
+            
+                    // Also remove any child states of this state.
+                    var tmpStates = new Array();
+                    var count = 0;
+                    for (var i = 0; i < newStates.length; i++) {
+                        if (newStates[i].indexOf(state+this.hierarchySeparator) != 0)
+                            tmpStates[count++] = newStates[i];
+                    }
+            
+                    $.stateBindings.setState(tmpStates);
+                    return true;
+                }
+                return false;
             },
             
             toggleState: function(state) {
                 if ($.stateBindings.activeStates.contains(state)) {
-                    $.stateBindings.setState(
-                        $.stateBindings.activeStates.subtract(state)
-                    );
+                    return $.stateBindings.removeState(state) === false;
                 } else {
-                    $.stateBindings.setState(
-                        $.stateBindings.activeStates.concat([state])
-                    );
+                    return $.stateBindings.addState(state);
                 }
+            },
+            
+            isEnabled: function(state) {
+                return $.stateBindings.activeStates.contains(state);
             }
         }
     });
